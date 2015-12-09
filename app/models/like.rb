87cbfd710e40f44aad94b5a07c47b5b32465db4e
Stephen <stephen@agilestyle.com>
2015-12-09 12:55:08 -0700

    Added before create/destroy methods to update a counter cache for the polymorphic relationship.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,19 @@
 class Like < ActiveRecord::Base
   belongs_to: :user
   belongs_to: :likeable, polymorphic: true
+
+  before_create  :increment_counter
+  before_destroy :decrement_counter
+
+private
+
+  # increments the right classifiable counter for the right taxonomy
+  def increment_counter
+    self.taxonomy_type.constantize.increment_counter("#{self.classifiable_type.downcase.pluralize}_count", self.taxonomy_id)
+  end
+
+  # decrements the right classifiable counter for the right taxonomy
+  def decrement_counter
+    self.taxonomy_type.constantize.decrement_counter("#{self.classifiable_type.downcase.pluralize}_count", self.taxonomy_id)
+  end
 end
