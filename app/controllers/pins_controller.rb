8bc8847076599142243363c95f6e13a7d0528728
Stephen <stephen@agilestyle.com>
2015-12-09 13:05:10 -0700

    Fixed indentation. Fixed association definitions.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -45,19 +45,19 @@ class PinsController < ApplicationController
     redirect_to pins_url
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
 
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    end
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description, :image, :board_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def pin_params
+    params.require(:pin).permit(:description, :image, :board_id)
+  end
 end
