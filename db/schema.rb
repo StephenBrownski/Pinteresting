85dd19b1782b534d7077534949e921711acbec14
Stephen <stephen@agilestyle.com>
2015-12-09 09:42:04 -0700

    Add a 'public' boolean to boards. Unless a board is marked as 'public', it will not be visible on the public boards page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203213250) do
+ActiveRecord::Schema.define(version: 20151208235236) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151203213250) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.boolean  "public"
   end
 
   add_index "boards", ["user_id"], name: "index_boards_on_user_id"
